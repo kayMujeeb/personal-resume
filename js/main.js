593ef4ba533a00723176f1eb1186be4740ce7307
Khurrum Abdul Mujeeb <Khurrum@Khurrums-MacBook-Pro.local>
2016-01-06 16:42:56 -0500

    Ready for demo - missing text

--- js/main.js
+++ js/main.js
@@ -3,24 +3,24 @@ $('.click-me').on('click', function() {
 	$(this).animate({transform: 'rotate(45deg)'});
 });
 
-$(window).on('resize', function () {
+$(window).on('resize', function() {
 	$(window).trigger('scroll');
 
-})
+});
 
 
 $(window).on('scroll', function() {
 
 	//Number of px that have been scrolled
 	var px = $(this).scrollTop();
-	console.log(px);
+	// console.log(px);
 
 	var h = $(window).height();
-	console.log(h);
+	// console.log(h);
 
 	//Calculate how much of the page has been scrolled
 	var percent = px/h;
-	console.log(percent);
+	// console.log(percent);
 
 	var op = 1 - percent;
 
@@ -33,9 +33,63 @@ $(window).on('scroll', function() {
 
 	if (px > 2) {
 		$('.bar').addClass('bar-background');
+		$('.bar-logo').fadeIn();
 	}
 	else {
 		$('.bar').removeClass('bar-background');
+		$('.bar-logo').fadeOut();
+	}
+
+	if (px > 640 && px < 1349) {
+		$('#i-do').addClass('active');
+		$('#ive-done').removeClass('active');
+	}
+
+	else if (px > 1349) {
+		$('#ive-done').addClass('active');
+		$('#i-do').removeClass('active');
+	}
+
+	else {
+		$('#ive-done').removeClass('active');
+		$('#i-do').removeClass('active');	
 	}
 	
+});
+
+
+// $('#i-do').on('click', function() {
+// 	$('#i-do').addClass('active');
+// 	$('#ive-done').removeClass('active');
+// })
+
+// $('#ive-done').on('click', function() {
+// 	$('#ive-done').addClass('active');
+// 	$('#i-do').removeClass('active');
+// })
+
+
+$(function () {
+    $('.portfolio-banner a').fluidbox();
+
+})
+
+$('.portfolio-banner a').fluidbox({
+    viewportFill: 0.8,
+    maxWidth: 800,
+    loader: true
+});
+
+
+$('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 600, 'swing', function () {
+        window.location.hash = target;
+    });
 });
